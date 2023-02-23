8ace9b2ff4ded45c78408bed66b89756857efb5d
MishC <michaela.chovancova@gmail.com>
2023-02-23 06:26:18 +0100

    new file

---
+++ src/content/ImageList/images_list/description.js
@@ -0,0 +1,63 @@
+export default {
+  creators: [
+    {
+      id: 1,
+      text: [
+        "PriorAttire is creating YouTube videos on historical fashions, dressmaking etc.",
+
+        "HandyMumLin is Sewing video maker",
+
+        "Kevin Wilson is telling stories over a cup of chai",
+
+        "Feli from Germany is creating YouTube videos",
+      ],
+    },
+    {
+      id: 2,
+      text: [
+        "AdamBoreland is Loving through Dance",
+
+        "MaremiSmallArt",
+
+        "Pet Artist Mary Carnot is Pet Artist",
+
+        "Hamilton Glass is painting to raise funds for community organizations",
+      ],
+    },
+    {
+      id: 3,
+      text: [
+        "L'Oreal Thompson Payton is writing motivational newsletters to get you through the midweek slump",
+        "Sharleen Joynt is making sure you always have something to read about The Bachelor",
+
+        "Somewhere: For Us is Scotland's leading LGBTQ+ magazine and funding scheme",
+
+        "Rebekah Borucki is building better ways to publish and amplify voices that matter",
+      ],
+    },
+    {
+      id: 4,
+      text: [
+        "Mike Barnes Drums is creating drum tuition videos on YouTube and other platforms",
+
+        "johnnathancordy is creating guitar music and hopefully useful lessons?",
+
+        "NicknLex is reacting to new music everyday",
+
+        "Ross Farley is making drum transcriptions and lessons",
+      ],
+    },
+    {
+      id: 5,
+      text: [
+        "Stephanie Eckles (5t3ph) is the author of ModernCSS.dev, StyleStage.dev, 11ty.Rocks",
+
+        "Luke Tsekouras is enabling fun with friends",
+
+        "TooLiPHoNe.NeT is customizing iOS without jailbreak 😎",
+
+        "CodeBeauty is a Software Engineer who teaches you how to write beautiful code! 💻&zwj;✨",
+      ],
+    },
+  ],
+};
