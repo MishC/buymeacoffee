cbf50a09ede5629ffa19ca5c690cf1c940845703
MishC <michaela.chovancova@gmail.com>
2023-02-23 16:06:03 +0100

    styling

--- src/content/startMyPage/startMyPage.jsx
+++ src/content/startMyPage/startMyPage.jsx
@@ -3,17 +3,17 @@ import "./startMyPage.css";
 
 const startMyPage = () => {
   return (
-    <div>
+    <div className="startMyPage">
       <h1>A supporter is worth a thousand followers.</h1>
       <h3>
         Accept donations. Start a membership. Sell anything you like. It’s
         easier than you think.{" "}
       </h3>
-      <div className="">
+      <div className="makePage border ">
         <span className="">buymeacoffee.com/</span>
-        <input placeholder="yourname" />
-        <button className="">
-          <span className="inline-flex relative">
+        <input className="" placeholder="yourname" />
+        <button className="btn">
+          <span className=" inline-flex relative">
             {" "}
             <h3>
               Start my page<span></span>
@@ -21,6 +21,7 @@ const startMyPage = () => {
           </span>
         </button>
       </div>
+      <h6>It’s free, and takes less than a minute.</h6>
     </div>
   );
 };
