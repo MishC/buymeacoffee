7511f75cd75768e885cffc018848b9d1b5edecac
MishC <michaela.chovancova@gmail.com>
2023-02-23 13:51:17 +0100

    stickydropdown

---
+++ src/content/Sticky/Dropdown.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Dropdown.css";
+
+function Dropdown({ name, links }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const closeDropdown = () => {
+    setShowDropdown(false);
+  };
+
+  return (
+    <div className="dropdown" onBlur={closeDropdown}>
+      <button onClick={toggleDropdown} className="dropdown-toggle">
+        {name}
+      </button>
+      {/* {links ? (
+        <ul className={`dropdown-menu ${showDropdown ? "show" : ""}`}>
+          {links.map((link, index) => (
+            <li key={index} style={{ maxWidth: "max-content" }}>
+              {link}
+            </li>
+          ))}
+        </ul>
+      ) : ( */}
+      {links ? (
+        <div className={`dropdown-menu ${showDropdown ? "show" : ""}`}>
+          {links}
+        </div>
+      ) : (
+        <div> </div>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
