c552cd7862a1e61018aac56f41406a6201832cee
MishC <michaela.chovancova@gmail.com>
2023-03-08 14:22:35 +0100

    route  to /yourpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./content/Home/Home";
 import SignUp from "./content/SignUp/signUp";
+import YourPage from "./content/yourPage/yourPage";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/yourpage" element={<YourPage />} />
           </Routes>
         </Router>
       </main>
