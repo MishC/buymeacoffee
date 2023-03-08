606e68f4be7e5e1e3d7ab4cba91984332831f9cf
MishC <michaela.chovancova@gmail.com>
2023-03-08 14:23:13 +0100

    Update sticky.jsx

--- src/content/Sticky/sticky.jsx
+++ src/content/Sticky/sticky.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./sticky.css";
 import Dropdown from "./Dropdown";
 import { name, linksSticky } from "./links_sticky.js";
