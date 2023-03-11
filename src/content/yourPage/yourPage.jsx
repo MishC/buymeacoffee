9490ef7b60884f7d4e46998955f972bbfd2f5d18
MishC <michaela.chovancova@gmail.com>
2023-03-11 12:13:35 +0100

    jsx

--- src/content/yourPage/yourPage.jsx
+++ src/content/yourPage/yourPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navigation from "../Navigation/Navigation";
+import Navigation from "../Navigation/Navigation.jsx";
 import { useNavigate } from "react-router-dom";
 
 import { useAuthState } from "react-firebase-hooks/auth";
